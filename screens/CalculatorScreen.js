39df8cdc6718cafe732d13dc95bcb735acb14b77
langlant <65258571+langlant@users.noreply.github.com>
2020-06-01 21:04:21 -0400

    Revert "HistoryScreen creation"
    This reverts commit fbf9d45774ce5ea68759ad726cebc49ad3ae01d3.

--- screens/CalculatorScreen.js
+++ screens/CalculatorScreen.js
@@ -3,7 +3,6 @@ import { StyleSheet, Text, Keyboard, TouchableOpacity, View, TouchableWithoutFee
 import { Button, Input } from "react-native-elements";
 import { Feather } from "@expo/vector-icons";
 import {initGeoCalcDb, writeData, setupDataListener} from '../helpers/fb-geocalculator';
-
 const CalculatorScreen = ({ route, navigation }) => {
   const [state, setState] = useState({
     lat1: "",
@@ -136,18 +135,6 @@ const CalculatorScreen = ({ route, navigation }) => {
         }
       >
         <Feather style={{ marginRight: 10 }} name="settings" size={24} color='#fff' />
-      </TouchableOpacity>
-    ),headerLeft: () => (
-      <TouchableOpacity
-        onPress={() => {
-          // navigate back with new settings.
-          navigation.navigate('History', {
-            selectedDistanceUnits,
-            selectedBearingUnits,
-          });
-        }}
-      >
-        <Text style={styles.headerButton}> Save </Text>
       </TouchableOpacity>
     ),
   });
