744ea20a44a420753ce8c3831ce77b389c66960c
langlant <65258571+langlant@users.noreply.github.com>
2020-06-03 14:58:32 -0400

    calc

--- helpers/fb-geocalculator.js
+++ helpers/fb-geocalculator.js
@@ -8,26 +8,14 @@ export function initGeoCalcDB()
 }
 
 export function writeData(key,data){
-    firebase.database.ref(`currentHistory/${key}`).set(data);
+    firebase.database.ref(`calculatorData/${key}`).set(data);
 }
 
-export function setupDataListener(updateItems) {
+export function setupDataListener(key){
     firebase
-    .database()
-    .ref(`calculatorData/`)
-    .on('value', (snapshot) => {
-        console.log('data listener fire up with : ', snapshot);
-        const newArr = [];
-        if(snapshot?.val()){
-            const items = snapshot.val();
-            Object.keys(items).map((key, index) => {
-                console.log(key, '||', index, '||', items[key]);
-                newArr.push({ ...items[key], id: key });
-              });
-        
-            updateItems(newArr);
-        }else {
-            updateItems([]);
-        }
-    });
+        .database()
+        .ref(`calculatorData/${key}`)
+        .on('value', (snapshot) => {
+            console.log('data listener fires up with: ', snapshot);
+        });
 }
